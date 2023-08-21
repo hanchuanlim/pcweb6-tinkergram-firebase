1497d4c159a3d8ad4eb0c540726f44d2757714c2
Han Chuan <hanchuan.lim@gmail.com>
2023-08-21 18:08:04 +0800

    Updated PostPostDetails and SignUpPage.js

--- src/views/PostPageDetails.js
+++ src/views/PostPageDetails.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Col, Container, Image, Nav, Navbar, Row } from "react-bootstrap";
-import { useParams, useNavigate, Await } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db, storage } from "../firebase";
 import { signOut } from "firebase/auth";
-import { deleteDoc, doc, collection, getDoc, getDocs, addDoc, setDoc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, collection, getDoc, getDocs, addDoc, setDoc, updateDoc, query, where } from "firebase/firestore";
 import { ref, deleteObject } from "firebase/storage";
 
 export default function PostPageDetails() {
@@ -30,11 +30,46 @@ export default function PostPageDetails() {
 
       if (likesCollectionSnapshot.empty) {
         // The "likes" sub-collection doesn't exist, create it
-        await setDoc(doc(likesCollectionRef), { like: [user.uid, user.email] });
+        await setDoc(doc(likesCollectionRef), {uid: user.uid, email: user.email, displayName: user.displayName});
+        setLikeCount(1);
+
       } else {
 
-        await setDoc(doc(likesCollectionRef), { like: [user.uid, user.email] });
-        setLikeCount(likesCollectionSnapshot.size);
+        const q = query(likesCollectionRef, where("uid", "==", user.uid));
+        const likesSnapshot = await getDocs(q);
+        const element = document.getElementById("like-link");
+
+        
+
+        if (likesSnapshot.empty) {
+          const likeDocRef = doc(likesCollectionRef); 
+          await setDoc(likeDocRef, {uid: user.uid, email: user.email, displayName: user.displayName});
+          // await setDoc(doc(likesCollectionRef), { like: [user.uid, user.email] });
+          setLikeCount(likesCollectionSnapshot.size+1);
+
+          // const element = document.getElementById("like-link");
+
+          // Add styles to the element
+          // element.style.color = "red";
+          // element.style.fontSize = "16px";
+          element.style.fontWeight = "bold";
+        }
+        else {
+          const likeDocRef = doc(likesCollectionRef, likesSnapshot.docs[0].id); 
+
+          try {
+            await deleteDoc(likeDocRef);
+            setLikeCount(likesCollectionSnapshot.size-1);
+
+          } catch (error) {
+            console.error(error.message);
+          }
+
+          
+
+        }
+
+      
       }
     }
     else {
@@ -67,20 +102,43 @@ export default function PostPageDetails() {
   }
 
   async function getPost(id) {
-    const postDocument = await getDoc(doc(db, "posts", id));
+    const postDocumentRef = doc(db, "posts", id);
+    const postDocument = await getDoc(postDocumentRef);
     const post = postDocument.data();
 
     setCaption(post.caption);
     setImage(post.image);
     setImageName(post.imageName);
     setComment(post.comment);
+
+
+    const likesCollectionRef = collection(postDocumentRef, "likes");
+    const likesCollectionSnapshot = await getDocs(likesCollectionRef);
+
+    if (!likesCollectionSnapshot.empty) {
+      setLikeCount(likesCollectionSnapshot.size);
+
+
+      const q = query(likesCollectionRef, where("uid", "==", user.uid));
+      const likesSnapshot = await getDocs(q);
+      
+      if (!likesSnapshot.empty) {
+        const element = document.getElementById("like-link");
+        element.style.fontWeight = "bold";
+
+      }
+
+
+    }
+
+
   }
 
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/login");
     getPost(id);
-  }, [id, loading, user, navigate]);
+  }, [id, loading, user, navigate, likeCount]);
 
 
   return (
@@ -119,6 +177,7 @@ export default function PostPageDetails() {
                 <Card.Link
                   onClick={() => likePost(id)}
                   style={{ cursor: "pointer" }}
+                  id="like-link"
                 >
                   Like 
                   {`(${likeCount})`}
--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
-import {createUserWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
 import {useNavigate} from "react-router-dom";
 import {auth} from "../firebase";
 
 export default function SignUpPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [displayname, setDisplayName] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -37,12 +38,31 @@ export default function SignUpPage() {
           />
           <a href="/login">Have an existing account? Login here.</a>
         </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicDisplayName">
+          <Form.Label>Display Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter display name"
+            value={displayname}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We will use this display name when displaying your public activities.
+          </Form.Text>
+        </Form.Group>
+
         <Button variant="primary" onClick={async (e) => {
             setError("");
-            const canSignUp = username && password;
+            const canSignUp = username && password && displayname;
             if (canSignUp) {
                 try {
-                    await createUserWithEmailAndPassword(auth, username, password);
+                    await createUserWithEmailAndPassword(auth, username, password)
+                    .then((userCredential) => {
+                      // Update the user's display name
+                      const user = userCredential.user;
+                      return updateProfile(user, { displayName: displayname });
+                    });
                     navigate("/");
 
                 } catch (err) {
